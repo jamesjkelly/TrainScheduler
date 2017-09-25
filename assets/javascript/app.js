be76ed1f49cd09bce474f7c5a0806c2d74bdf770
jamesjkelly <jkellynjus@hotmail.com>
2017-09-24 22:07:55 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,18 @@
 //app code here
+$(document).ready(function() {
+
+    $("#getTrain").click(function(event) {
+        event.preventDefault();
+        console.log('you clicked me');
+        var tTable = $("#trainTable");
+        var tForm = $("#trainForm");
+        var tName = $("#trainName").val();
+        var dest = $("#destination").val();
+        var tTime = $("#trainTime").val();
+        var freq = $("#frequency").val();
+        var today = new Date();
+
+        $("#trainTable").append('<tr><td>' + tName + '</td><td>' + dest + '</td><td>' + tTime + '</td><td>' + freq + '</td><td>' + today + '</td></tr>');
+    });
+
+});
