fe5acf71220ebd36e5723f4f3a9ceffc6b9fe772
jamesjkelly <jkellynjus@hotmail.com>
2017-09-26 23:30:33 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,11 +21,7 @@ $(document).ready(function() {
   database.ref().on("child_added", function(snapshot) {
     var current = snapshot.val();
     var now = moment();
-    var tStart = moment(current.time, "HH:mm");
-    // NA = ((CT-ST)/FR + 1) * FR + ST
-    // MA = NA - CT
-    var nextTrain = moment(tStart.add((Math.ceil((now.diff(tStart, "minutes")/current.freq)) * current.freq), "minutes"));
-
+  
     var minAway = nextTrain.diff(now, "minutes");
 
     var timediff = moment().diff(moment(current.time, "HH:mm"), "minutes");
@@ -36,7 +32,7 @@ $(document).ready(function() {
     }</td><td>${
       current.freq
     }</td><td>${
-      nextTrain.format("HH:mm")
+    
     }</td><td>${
       minAway
     }</td></tr>`);
