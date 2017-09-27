f354711682a5f76da4ff658aeb98f9a1cf5a5607
jamesjkelly <jkellynjus@hotmail.com>
2017-09-26 23:15:20 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,44 @@
-//app code here
+
 $(document).ready(function() {
+  var $table = $("#train-table");
+  var $form = $("#train-form");
+  var $name = $("#train-name");
+  var $dest = $("#destination");
+  var $time = $("#train-time");
+  var $freq = $("#frequency");
+  var database = firebase.database();
 
-    $("#getTrain").click(function(event) {
-        event.preventDefault();
-        console.log('you clicked me');
-        var tTable = $("#trainTable");
-        var tForm = $("#trainForm");
-        var tName = $("#trainName").val();
-        var dest = $("#destination").val();
-        var tTime = $("#trainTime").val();
-        var freq = $("#frequency").val();
-        var today = new Date();
+  $form.submit(function(event) {
+    event.preventDefault();
+    database.ref().push({
+      name: $name.val(),
+      dest: $dest.val(),
+      time: $time.val(),
+      freq: $freq.val()
+    })
+  });
 
-        $("#trainTable").append('<tr><td>' + tName + '</td><td>' + dest + '</td><td>' + tTime + '</td><td>' + freq + '</td><td>' + today + '</td></tr>');
-    });
+  database.ref().on("child_added", function(snapshot) {
+    var current = snapshot.val();
+    var now = moment();
+    var tStart = moment(current.time, "HH:mm");
+    // NA = ((CT-ST)/FR + 1) * FR + ST
+    // MA = NA - CT
+    var nextTrain = moment(tStart.add((Math.ceil((now.diff(tStart, "minutes")/current.freq)) * current.freq), "minutes"));
 
+    var minAway = nextTrain.diff(now, "minutes");
+
+    var timediff = moment().diff(moment(current.time, "HH:mm"), "minutes");
+    $table.append(`<tr><td>${
+      current.name
+    }</td><td>${
+      current.dest
+    }</td><td>${
+      current.freq
+    }</td><td>${
+      nextTrain.format("HH:mm")
+    }</td><td>${
+      minAway
+    }</td></tr>`);
+  });
 });
